086a8f58c58d2dec3f8f89fa2ab80753beb669eb
Son Bui <sonbv00@gmail.com>
2017-07-14 00:31:07 +0700

    simpleArraySum solution

---
+++ Warmup/simple_array_sum.py
@@ -0,0 +1,7 @@
+#!/bin/python
+
+import sys
+from functools import reduce
+
+def simpleArraySum(n, ar):
+	return sum(ar[:n])
---
+++ Warmup/simple_array_sum_test.py
@@ -0,0 +1,22 @@
+import unittest
+from simple_array_sum import simpleArraySum
+
+class TestSimpleArrayFunction(unittest.TestCase):
+	def test_simple_array_function(self):
+		data = [
+			[31, 6, [1, 2, 3, 4, 10, 11]]
+			,[0, 4, [0, 0, 0, 0]]
+			,[40, 6, [2, 6, 3, 4, 10, 15]]
+			,[31, 6, [1, 2, 3, 4, 10, 11]]
+			# Not normal case here. when n <> number array items
+			,[21, 6, [1, 2, 3, 4, 11]]
+			,[25, 5, [2, 6, 3, 4, 10, 15]]
+		]
+		for item in data:
+			self.assertEqual(
+				item[0] 
+				,simpleArraySum(item[1], item[2])
+				,"Test simpleArraySum with n = {0} and ar = {1}, expect return value is {2}"
+						.format(item[1], item[2], item[0])
+			)
+
